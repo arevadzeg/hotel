04136ec1f2417e02be3b8204bf97476db8f9b718
Giorgi Arevadze <gioarevadze97@gmail.com>
2022-09-19 12:28:17 +0400

    created products list page

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,9 +1,42 @@
-
+import { useState, useEffect } from "react"
+import config from "../../config"
+import './productList.css'
 
 const ProductList = () => {
 
-    return <div>
-        Product list
+    const [styles, setStyles] = useState(null)
+    const [products, setProducts] = useState([])
+    const [productsListHeader, setProductsListHeader] = useState(null)
+
+    useEffect(() => {
+        const { list, productsListHeader, ...styles } = config.productList
+        setProducts(list)
+        setStyles(styles)
+        setProductsListHeader(productsListHeader)
+
+    }, [])
+    console.log(products)
+
+    return <div className="products_list-wrapper">
+        <video src={config.productList.backgroundVideo} autoPlay muted loop />
+        {productsListHeader && <h2 style={productsListHeader.styles}>{productsListHeader.text}</h2>}
+        {
+            products.map((product, i) => {
+                return <div className="product_wrapper" key={i}>
+                    <div className="product_description" style={styles.postStyles}>
+                        <h3 style={styles.postHeader}>{product.header}</h3>
+                        <p style={styles.postDescription}>{product.description}</p>
+                        {
+                            product.button.enable && <button className="product_button">
+                                {product.button.text}
+                            </button>
+                        }
+                    </div>
+                    <img src={product.img} alt=" " />
+
+                </div>
+            })
+        }
     </div>
 }
 
--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 
 const config = {
     header: {
-        color: '#000',
+        color: '#fff',
         enable: true,
-        backgroundColor: '#ccc',
+        backgroundColor: '#000',
         fontWeight: 500,
         fontSize: "16px",
         padding: '15px 20px',
-        logo: 'https://onlinepngtools.com/images/examples-onlinepngtools/google-logo-transparent.png',
+        logo: 'https://roomshotels.com/wp-content/themes/roomshotelsv2/img/logo-secondary-white.svg',
         navItems: [
             {
                 displayText: 'About us',
@@ -26,9 +26,68 @@ const config = {
                 link: '/gallery'
             }
         ]
+    },
 
+    productList: {
+        backgroundVideo: "https://roomshotels.com/wp-content/uploads/2021/09/Rooms-Tbilisi-1600px_s.mp4",
+        postStyles: {
+            backgroundColor: "#fff",
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: '4vw'
+        },
+        postHeader: {
+            fontSize: "25px",
+            color: "#000",
+            fontWeight: 600,
+        },
+        postDescription: {
+            fontSize: "20px",
+            color: "#000",
+            fontWeight: 400,
+        },
+        productsListHeader: {
+            text: 'Original Experiences in Tbilisi',
+            styles: {
+                fontSize: "48px",
+                color: "#000",
+                fontWeight: 400,
+                textAlign: 'center',
+                margin: '90px'
+            }
+        },
+        list: [
+            {
+                img: 'https://roomshotels.com/wp-content/uploads/2021/09/rh-tbilisi-kitchen-entrance-1600x1068.webp',
+                header: 'Modern Retellings of Old Traditions',
+                description: 'The Kitchen represents the latest phase of Rooms Hotel Tbilisi’s culinary journey. Located on the ground floor overlooking the garden, the restaurant reflects the hotel’s signature aesthetic with a blend of traditional and modern design features. Head chef David Legrand, inspired by the world’s hottest spots of culinary, fits his experience with the identity of Rooms Hotel.',
+                button: {
+                    enable: true,
+                    text: 'View The Kitchen',
+                    link: '/kitchen'
+                }
+            },
+            {
+                img: 'https://roomshotels.com/wp-content/uploads/2021/09/rh-tbilisi-bar-room-cocktail.webp',
+                header: 'Bar Room',
+                description: 'The Bar and Lounge are Tbilisis most authentic social and meeting spaces during the day, while turning into an inviting evening spot after the dinner',
+                button: {
+                    enable: false,
+                }
+            },
+            {
+                img: 'https://roomshotels.com/wp-content/uploads/2021/09/rh-tbilisi-garden-bar-1600x1068.webp',
+                header: 'Garden Bar',
+                description: 'The ultimate urban retreat, the Garden Bar lights up the eastern corner of the hotel’s courtyard garden.s',
+                button: {
+                    enable: false,
+                }
+            }
 
+        ]
     }
+
+
 }
 
 
